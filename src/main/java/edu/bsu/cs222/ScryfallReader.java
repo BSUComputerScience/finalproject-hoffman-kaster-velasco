9f744f8c46a894c97c378d3dbe57b5e81630cc9d
GioGote <sivelasco.cs@gmail.com>
2022-10-20 13:46:26 -0400

    Gio Velasco <sivelasco@bsu.edu> Build Card and Reader Class should be able to ping Scryfall API now.

---
+++ src/main/java/edu/bsu/cs222/ScryfallReader.java
@@ -0,0 +1,30 @@
+package edu.bsu.cs222;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URLConnection;
+import java.net.URL;
+
+public class ScryfallReader {
+
+    public static InputStream getScryfallStream(URL encodedUrl) throws IOException {
+        URLConnection connection = encodedUrl.openConnection();
+        connection.setRequestProperty("User-Agent", "ScryfallReader/0.1 sivelasco@bsu.edu");
+        return connection.getInputStream();
+    }
+
+    public static URL encodeURL (String cardName){
+        String scryfallCardName = new String(cardName.replaceAll(" ","+"));
+        String urlString = String.format("api.scryfall.com/cards/named?fuzzy=%s", scryfallCardName);
+        URL url = null;
+        try {
+            url = new URL(urlString.replaceAll(" ", "%20"));
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+}
