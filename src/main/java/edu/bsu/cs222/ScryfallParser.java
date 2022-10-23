7208b4650a6a11535b77bafc8ca054ba9a665889
North Kaster <northkaster@gmail.com>
2022-10-23 19:20:24 -0400

    Add more attributes to parser

--- src/main/java/edu/bsu/cs222/ScryfallParser.java
+++ src/main/java/edu/bsu/cs222/ScryfallParser.java
@@ -8,8 +8,17 @@ import java.io.InputStream;
 
 public class ScryfallParser {
     public String parse(InputStream testDataStream) throws IOException {
-        JSONArray cardData = JsonPath.read(testDataStream, "$..name");
-
-        return cardData.get(0).toString();
+        JSONArray cardData = JsonPath.read(testDataStream, "$.*");
+        JSONArray cardName = JsonPath.read(cardData, "$..name");
+        JSONArray cardManaCost = JsonPath.read(cardData, "$..mana_cost");
+        JSONArray cardType = JsonPath.read(cardData, "$..set_type");
+        JSONArray cardRarity = JsonPath.read(cardData, "$..rarity");
+        JSONArray cardAbilities = JsonPath.read(cardData, "$..oracle_text");
+        JSONArray cardFlavorText = JsonPath.read(cardData, "$..flavor_text");
+        JSONArray cardPower = JsonPath.read(cardData, "$..power");
+        JSONArray cardToughness = JsonPath.read(cardData, "$..toughness");
+        JSONArray cardColors = JsonPath.read(cardData, "$..colors");
+        System.out.println(cardName.get(0));
+        return cardName.get(0).toString();
     }
 }
