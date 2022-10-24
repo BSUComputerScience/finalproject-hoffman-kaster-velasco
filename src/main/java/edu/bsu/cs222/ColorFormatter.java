b205a6ae0f2c2a5b05f4cf02e163dc20cf03bda1
North Kaster <northkaster@gmail.com>
2022-10-24 16:50:56 -0400

    Create ColorFormatter and colorFormatterTest. Test FAILS

---
+++ src/main/java/edu/bsu/cs222/ColorFormatter.java
@@ -0,0 +1,7 @@
+package edu.bsu.cs222;
+
+public class ColorFormatter {
+    public String format(String colorsTest) {
+        return "";
+    }
+}
---
+++ src/test/java/edu/bsu/cs222/ColorFormatterTest.java
@@ -0,0 +1,17 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ColorFormatterTest {
+
+    @Test
+    public void ColorFormatterTest(){
+        ColorFormatter colorFormatterTest = new ColorFormatter();
+        String colorsTest = new String ("{W}{U}{B}{R}{G}");
+        String colorFormatterResult = colorFormatterTest.format(colorsTest);
+        Assertions.assertEquals("White, Blue, Black, Red, Green", colorFormatterResult);
+
+
+    }
+}
