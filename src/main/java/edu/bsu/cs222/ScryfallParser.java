f0c931d0cafe1a4995e4d9b79c7838e2058817b0
North Kaster <northkaster@gmail.com>
2022-10-24 16:35:23 -0400

    Add flavorText check, convert all Arrays to strings

--- src/main/java/edu/bsu/cs222/ScryfallParser.java
+++ src/main/java/edu/bsu/cs222/ScryfallParser.java
@@ -7,17 +7,37 @@ import java.io.InputStream;
 
 public class ScryfallParser {
     public String parse(InputStream testDataStream) throws IOException {
+        //Turn inputstream into String for re-use
         String cardData = new String(testDataStream.readAllBytes());
 
-        JSONArray cardName = JsonPath.read(cardData, "$..name");
-        JSONArray cardManaCost = JsonPath.read(cardData, "$..mana_cost");
-        JSONArray cardType = JsonPath.read(cardData, "$..set_type");
-        JSONArray cardRarity = JsonPath.read(cardData, "$..rarity");
-        JSONArray cardAbilities = JsonPath.read(cardData, "$..oracle_text");
-        JSONArray cardFlavorText = JsonPath.read(cardData, "$..flavor_text");
-        JSONArray cardPower = JsonPath.read(cardData, "$..power");
-        JSONArray cardToughness = JsonPath.read(cardData, "$..toughness");
-        JSONArray cardColors = JsonPath.read(cardData, "$..colors");
-        return cardName.get(0).toString();
+        //Parse cardData String for attributes, place in JSONArray
+        JSONArray cardNameArray = JsonPath.read(cardData, "$..name");
+        JSONArray cardManaCostArray = JsonPath.read(cardData, "$..mana_cost");
+        JSONArray cardTypeArray = JsonPath.read(cardData, "$..set_type");
+        JSONArray cardRarityArray = JsonPath.read(cardData, "$..rarity");
+        JSONArray cardAbilitiesArray = JsonPath.read(cardData, "$..oracle_text");
+        JSONArray cardFlavorTextArray = JsonPath.read(cardData, "$..flavor_text");
+        JSONArray cardPowerArray = JsonPath.read(cardData, "$..power");
+        JSONArray cardToughnessArray = JsonPath.read(cardData, "$..toughness");
+        JSONArray cardColorsArray = JsonPath.read(cardData, "$..colors");
+
+        //Convert JSONArrays to Strings
+        String cardName = cardNameArray.get(0).toString();
+        String cardManaCost = cardManaCostArray.get(0).toString();
+        String cardType = cardTypeArray.get(0).toString();
+        String cardRarity = cardRarityArray.get(0).toString();
+        String cardAbilities = cardAbilitiesArray.get(0).toString();
+        String cardPower = cardPowerArray.get(0).toString();
+        String cardToughness = cardToughnessArray.get(0).toString();
+        String cardColors = cardColorsArray.get(0).toString();
+        String cardFlavorText = "";
+
+        //Only convert FlavorText to String if there is something in the cardFlavorTextArray
+        if (cardFlavorTextArray.isEmpty()) {
+        } else {
+            cardFlavorText = cardFlavorTextArray.get(0).toString();
+        }
+
+        return cardName;
     }
 }
