bdcc2b96af63517e6c3a03bf20ad23ce4fdab862
North Kaster <northkaster@gmail.com>
2022-10-24 19:53:47 -0400

    Finish colorFormatter. Test Passes

--- src/main/java/edu/bsu/cs222/ColorFormatter.java
+++ src/main/java/edu/bsu/cs222/ColorFormatter.java
@@ -2,6 +2,13 @@ package edu.bsu.cs222;
 
 public class ColorFormatter {
     public String format(String colorsTest) {
-        return "";
+        String white = colorsTest.replace("{W}","White, ");
+        String blue = white.replace("{U}", "Blue, ");
+        String black = blue.replace("{B}","Black, ");
+        String red = black.replace("{R}","Red, ");
+        String green = red.replace("{G}","Green, ");
+        String formattedCardColors = green.substring(0, green.length()-2);
+        System.out.println(formattedCardColors);
+        return formattedCardColors;
     }
 }
