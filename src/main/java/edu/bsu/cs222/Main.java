ba831f30168e349b4cd7395449e6b794be0dfa91
North Kaster <northkaster@gmail.com>
2022-10-26 13:23:49 -0400

    Refactor in Main

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -33,8 +33,8 @@ public class Main {
     Card getCardInfo(String userInputtedCardName) throws IOException {
         URL encodedURL = ScryfallReader.encodeURL(userInputtedCardName);
         InputStream cardDataStream = ScryfallReader.getScryfallStream(encodedURL);
-        ScryfallParser parser = new ScryfallParser();
-        return parser.parse(cardDataStream);
+        ScryfallParser scryfallParser = new ScryfallParser();
+        return scryfallParser.parse(cardDataStream);
 
     }
 }
