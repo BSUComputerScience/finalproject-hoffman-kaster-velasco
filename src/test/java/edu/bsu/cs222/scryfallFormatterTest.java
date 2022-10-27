ac2f08153c322b35989ab93d373e70f0b007292a
North-Kaster <bckaster@bsu.edu>
2022-10-27 12:40:42 -0400

    Update scryfallFormatterTest to match new desired format output

--- src/test/java/edu/bsu/cs222/scryfallFormatterTest.java
+++ src/test/java/edu/bsu/cs222/scryfallFormatterTest.java
@@ -17,13 +17,14 @@ public class scryfallFormatterTest {
         cardList[0] = card;
         String formattedTestDataStream = scryfallFormatter.formatJson(cardList);
         Assertions.assertEquals("""
-                {2}{White}{Blue}
-                Legendary Artifact - Vehicle
-                Mythic
-                {1}, {T}: Draw two cards, then discard a card. Create a 1/1 colorless Pilot creature token with \"This creature crews Vehicles as though its power were 2 greater.\"\nCrew 8 (Tap any number of creatures you control with total power 8 or more: This Vehicle becomes an artifact creature until end of turn.)\nShorikai, Genesis Engine can be your commander.
-                                
-                8
-                8
-                Blue, White""", formattedTestDataStream);
+                           Converted Mana Cost:{2}{White}{Blue}
+                           Type: Legendary Artifact - Vehicle
+                           Rarity: Mythic
+                           Abilities: {1}, {T}: Draw two cards, then discard a card. Create a 1/1 colorless Pilot creature token with "This creature crews Vehicles as though its power were 2 greater."
+                           Crew 8 (Tap any number of creatures you control with total power 8 or more: This Vehicle becomes an artifact creature until end of turn.)
+                           Shorikai, Genesis Engine can be your commander.
+                           Power: 8
+                           Toughness: 8
+                           Colors: Blue, White""", formattedTestDataStream);
     }
 }
