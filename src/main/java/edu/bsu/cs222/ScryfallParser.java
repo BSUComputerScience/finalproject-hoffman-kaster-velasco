6ebdd787fae6cc9c939c2b6209541c826c904863
North-Kaster <bckaster@bsu.edu>
2022-11-01 12:54:03 -0400

    Add loyalty attribute and check to parser

--- src/main/java/edu/bsu/cs222/ScryfallParser.java
+++ src/main/java/edu/bsu/cs222/ScryfallParser.java
@@ -1,5 +1,4 @@
 package edu.bsu.cs222;
-
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
 import java.io.IOException;
@@ -13,13 +12,14 @@ public class ScryfallParser {
         //Parse cardData String for attributes, place in JSONArray
         JSONArray cardNameArray = JsonPath.read(cardData, "$..name");
         JSONArray cardManaCostArray = JsonPath.read(cardData, "$..mana_cost");
-        JSONArray cardTypeArray = JsonPath.read(cardData, "$..set_type");
+        JSONArray cardTypeArray = JsonPath.read(cardData, "$..type_line");
         JSONArray cardRarityArray = JsonPath.read(cardData, "$..rarity");
         JSONArray cardAbilitiesArray = JsonPath.read(cardData, "$..oracle_text");
         JSONArray cardFlavorTextArray = JsonPath.read(cardData, "$..flavor_text");
         JSONArray cardPowerArray = JsonPath.read(cardData, "$..power");
         JSONArray cardToughnessArray = JsonPath.read(cardData, "$..toughness");
         JSONArray cardColorsArray = JsonPath.read(cardData, "$..colors");
+        JSONArray cardLoyaltyArray = JsonPath.read(cardData, "$..loyalty");
 
         //Convert JSONArrays to Strings
         String name = cardNameArray.get(0).toString();
@@ -27,18 +27,32 @@ public class ScryfallParser {
         String type = cardTypeArray.get(0).toString();
         String rarity = cardRarityArray.get(0).toString();
         String abilities = cardAbilitiesArray.get(0).toString();
-        String power = cardPowerArray.get(0).toString();
-        String toughness = cardToughnessArray.get(0).toString();
+        String power = "";
+        String toughness = "";
         String colors = cardColorsArray.get(0).toString();
         String flavorText = "";
+        String loyalty = "";
 
-        //Only convert FlavorText to String if there is something in the cardFlavorTextArray
+        //Only convert FlavorText, power, and toughness to String if there is something in the array
         if (cardFlavorTextArray.isEmpty()) {
         } else {
             flavorText = cardFlavorTextArray.get(0).toString();
         }
+        if (cardPowerArray.isEmpty()) {
+        } else {
+            power = cardPowerArray.get(0).toString();
+        }
+        if (cardToughnessArray.isEmpty()) {
+        } else {
+            toughness = cardToughnessArray.get(0).toString();
+        }
+        if (cardLoyaltyArray.isEmpty()) {
+        } else {
+            loyalty = cardLoyaltyArray.get(0).toString();
+        }
 
-        Card cardList = new Card(name, convertedManaCost, type, rarity, abilities, flavorText, power, toughness, colors);
-        return cardList;
+
+        return new Card(name, convertedManaCost, type, rarity, abilities, flavorText, power, toughness, colors);
+
     }
 }
