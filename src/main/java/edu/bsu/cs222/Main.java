acc5e2a946537b27f12003986bb9bc3189a1c528
North-Kaster <bckaster@bsu.edu>
2022-11-01 13:38:14 -0400

    Add check for empty input in main

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -8,20 +8,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Main scryTutor = new Main();
+        boolean isUserInputEmpty = false;
 
-        System.out.println("Please enter the name of a Magic: The Gathering card");
 
-        Scanner userInputScanner = new Scanner(System.in);
-        String userInputtedCardName = userInputScanner.nextLine();
+        do {
+            System.out.println("Please enter the name of a Magic: The Gathering card");
+            Scanner userInputScanner = new Scanner(System.in);
+            String userInputtedCardName = userInputScanner.nextLine();
+            if (userInputtedCardName.isEmpty()) {
+                System.out.println("Nothing entered. Please try again");
+                isUserInputEmpty = true;
+            } else {
+                Card cardInfo = scryTutor.getCardInfo(userInputtedCardName);
 
-        Card cardInfo = scryTutor.getCardInfo(userInputtedCardName);
+                System.out.println("Card name: " + cardInfo.getCardName());
 
-        System.out.println("Card name: " + cardInfo.getCardName());
+                String formattedCardData = ScryfallFormatter.formatJson(new Card[]{cardInfo});
 
-        String formattedCardData = ScryfallFormatter.formatJson(new Card[]{cardInfo});
-
-        System.out.println("\n" + formattedCardData);
+                System.out.println("\n" + formattedCardData);
+            }
+        }
+        while (isUserInputEmpty) ;
     }
+
     Card getCardInfo(String userInputtedCardName) throws IOException {
         URL encodedURL = ScryfallReader.encodeURL(userInputtedCardName);
         InputStream cardDataStream = ScryfallReader.getScryfallStream(encodedURL);
