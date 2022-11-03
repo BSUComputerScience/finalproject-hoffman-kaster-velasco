569b3734286b70d9f57bf81cdf1a74e41b079891
GioGote <sivelasco.cs@gmail.com>
2022-11-03 13:34:07 -0400

    Gio Velasco <sivelasco@bsu.edu> USD and Loyalty are now showing up for cards

--- src/main/java/edu/bsu/cs222/ScryfallFormatter.java
+++ src/main/java/edu/bsu/cs222/ScryfallFormatter.java
@@ -9,15 +9,24 @@ public class ScryfallFormatter {
         String formattedCard = "";
         ColorFormatter colorFormatter = new ColorFormatter();
 
-        if (cardList[0].getCardFlavorText().isBlank()) {
+        if (cardList[0].getCardFlavorText().isBlank() && cardList[0].getCardLoyalty().isBlank()) {
             formattedCard = formattedCard + "Converted Mana Cost:" + colorFormatter.format(cardList[0].getConvertedManaCost())
                     + "\n" + "Type: " + cardList[0].getCardType()
                     + "\n" + "Rarity: " + cardList[0].getCardRarity()
                     + "\n" + "Abilities: " + cardList[0].getCardAbilities()
                     + "\n" + "Power: " + cardList[0].getCardPower()
                     + "\n" + "Toughness: " + cardList[0].getCardToughness()
-                    + "\n" + "Colors: " + colorFormatter.format(cardList[0].getCardColors());
-        }else{
+                    + "\n" + "Colors: " + colorFormatter.format(cardList[0].getCardColors())
+                    + "\n" + "Price (USD): $" + cardList[0].getCardUsd();
+        } else if (!cardList[0].getCardLoyalty().isBlank()) {
+            formattedCard = formattedCard + "Converted Mana Cost:" + colorFormatter.format(cardList[0].getConvertedManaCost())
+                    + "\n" + "Type: " + cardList[0].getCardType()
+                    + "\n" + "Rarity: " + cardList[0].getCardRarity()
+                    + "\n" + "Abilities: " + cardList[0].getCardAbilities()
+                    + "\n" + "Colors: " + colorFormatter.format(cardList[0].getCardColors())
+                    + "\n" + "Loyalty: " + cardList[0].getCardLoyalty()
+                    + "\n" + "Price (USD): $" + cardList[0].getCardUsd();
+        } else {
             formattedCard = formattedCard + "Converted Mana Cost:" + colorFormatter.format(cardList[0].getConvertedManaCost())
                     + "\n" + "Type: " + cardList[0].getCardType()
                     + "\n" + "Rarity: " + cardList[0].getCardRarity()
@@ -25,7 +34,8 @@ public class ScryfallFormatter {
                     + "\n" + "Flavor Text: " + cardList[0].getCardFlavorText()
                     + "\n" + "Power: " + cardList[0].getCardPower()
                     + "\n" + "Toughness: " + cardList[0].getCardToughness()
-                    + "\n" + "Colors: " + colorFormatter.format(cardList[0].getCardColors());
+                    + "\n" + "Colors: " + colorFormatter.format(cardList[0].getCardColors())
+                    + "\n" + "Price (USD): $" + cardList[0].getCardUsd();
         }
         return formattedCard;
     }
