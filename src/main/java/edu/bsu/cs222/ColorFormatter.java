cc9dcfc3d0c092c93c3e8bad01d17f8aabb7b49f
North-Kaster <bckaster@bsu.edu>
2022-11-10 14:00:09 -0500

    Make ColorFormatter check for empty color return ("[]")

--- src/main/java/edu/bsu/cs222/ColorFormatter.java
+++ src/main/java/edu/bsu/cs222/ColorFormatter.java
@@ -1,15 +1,16 @@
 package edu.bsu.cs222;
 
 public class ColorFormatter {
-    public String format(String colorsTest) {
-        String white = colorsTest.replace("W" ,"White");
+    public String format(String cardColors) {
+
+        if (cardColors.equals("[]")){
+            return "";
+        }
+        String white = cardColors.replace("W" ,"White");
         String black = white.replace("B","Black");
         String blue = black.replace("U", "Blue");
         String red = blue.replace("R","Red");
         String green = red.replace("G","Green");
-        //String formattedColors = green.replaceAll("[{}]", "");
-        String formattedCardColors = green.substring(0, green.length());
-        //System.out.println(formattedCardColors);
-        return formattedCardColors;
+        return green.replaceAll("[\\[\\]]", "");
     }
 }
