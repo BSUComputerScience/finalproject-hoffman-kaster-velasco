c92e14234b6fa0afa5ed3d2233de2ac03a67b086
North-Kaster <bckaster@bsu.edu>
2022-11-13 18:03:06 -0500

    Adjust ColorFormatter for more desirable output

--- src/test/java/edu/bsu/cs222/ColorFormatterTest.java
+++ src/test/java/edu/bsu/cs222/ColorFormatterTest.java
@@ -8,9 +8,9 @@ public class ColorFormatterTest {
     @Test
     public void ColorFormatterTest(){
         ColorFormatter colorFormatterTest = new ColorFormatter();
-        String colorsTest = "{W}{U}{B}{R}{G}";
+        String colorsTest = "\"W\",\"U\",\"B\",\"R\",\"G\"";
         String colorFormatterResult = colorFormatterTest.format(colorsTest);
-        Assertions.assertEquals("{White}{Blue}{Black}{Red}{Green}", colorFormatterResult);
+        Assertions.assertEquals("\"White\",\"Blue\",\"Black\",\"Red\",\"Green\"", colorFormatterResult);
 
 
     }
