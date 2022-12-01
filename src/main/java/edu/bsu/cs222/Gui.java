7e788c9aa79b55e76876a8b4b4220ff977d0636f
North-Kaster <bckaster@bsu.edu>
2022-12-01 15:41:48 -0500

    Add toggleButton to toggle dark mode in GUI.

--- src/main/java/edu/bsu/cs222/Gui.java
+++ src/main/java/edu/bsu/cs222/Gui.java
@@ -4,10 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -22,16 +19,18 @@ import java.io.IOException;
 
 public class Gui extends Application{
 
-    Text sceneTitle;
+    GridPane grid = new GridPane();
     Label description;
+    Text sceneTitle;
     TextField cardToCheck;
     Button checkButton;
+    ToggleButton darkModeButton;
     Text cardTitle;
     Text cardAttributes;
     Hyperlink hpl;
     String cardLink;
     String cardImgUrl;
-    GridPane grid = new GridPane();
+
     @Override
     public void start(Stage stage) {
         VBox vbox = new VBox();
@@ -41,9 +40,11 @@ public class Gui extends Application{
         grid.setVgap(15);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
+
         sceneTitle = new Text("ScryTutor Card Search");
         sceneTitle.setFont(Font.font("Consolas", FontWeight.NORMAL, 20));
         grid.add(sceneTitle, 0, 0, 2, 1);
+
         description = new Label("Enter Card Name: ");
         description.setFont(Font.font("Consolas", FontWeight.NORMAL, 12));
         grid.add(description, 0, 1);
@@ -53,6 +54,12 @@ public class Gui extends Application{
         checkButton.setFont(Font.font("Consolas"));
         checkButton.setDefaultButton(true);
         grid.add(checkButton, 1, 4);
+
+        darkModeButton = new ToggleButton("Dark Mode");
+        darkModeButton.setFont(Font.font("Consolas"));
+        darkModeButton.setSelected(false);
+        grid.add(darkModeButton, 3, 0);
+
         cardTitle = new Text();
         grid.add(cardTitle,1,6);
         cardAttributes = new Text();
@@ -67,9 +74,15 @@ public class Gui extends Application{
                 error.printStackTrace();
             }
         });
+        darkModeButton.setOnAction(event -> {
+            if (darkModeButton.isSelected()){
+                setDarkMode();
+            } else {
+                setLightMode();
+            }
+        });
 
-
-        Scene scene = new Scene(grid, 440, 240);
+        Scene scene = new Scene(grid, 800, 300);
         stage.setMaximized(true);
         stage.setTitle("ScryTutor - Magic: The Gathering Card Database");
         HBox hbox = new HBox(8);
@@ -83,13 +96,17 @@ public class Gui extends Application{
     private void handleButtonClick() throws IOException {
         String userEntry = cardToCheck.getText();
         if (userEntry.isEmpty()) {
+            removeCardData();
             cardTitle.setFill(Color.FIREBRICK);
             cardTitle.setText("No Card Name Was Entered");
             cardAttributes.setText("");
+
         } else {
             Main scryTutor = new Main();
+
             Card cardInfo = scryTutor.getCardInfo(userEntry);
-            cardTitle.setFill(Color.BLACK);
+            autoSetTextColor(cardTitle);
+
             cardTitle.setText(cardInfo.getCardName());
             String formattedCardAttributes = ScryfallFormatter.formatJson(new Card[]{cardInfo});
             cardAttributes.setText(formattedCardAttributes);
@@ -112,9 +129,52 @@ public class Gui extends Application{
         }
     }
 
-    private void showError(IOException error){
+    private void setDarkMode(){
+        grid.setBackground(new Background(new BackgroundFill(Color.web("#373737"), CornerRadii.EMPTY, Insets.EMPTY)));
+        if (cardTitle.getFill().equals(Color.FIREBRICK)){
+            sceneTitle.setFill(Color.WHITE);
+            description.setTextFill(Color.WHITE);
+            cardAttributes.setFill(Color.WHITE);
+        }
+        else {
+            sceneTitle.setFill(Color.WHITE);
+            description.setTextFill(Color.WHITE);
+            cardTitle.setFill(Color.WHITE);
+            cardAttributes.setFill(Color.WHITE);
+        }
+    }
+
+    private void setLightMode(){
+        grid.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        if (cardTitle.getFill().equals(Color.FIREBRICK)){
+            sceneTitle.setFill(Color.BLACK);
+            description.setTextFill(Color.BLACK);
+            cardAttributes.setFill(Color.BLACK);
+        }
+        else {
+            sceneTitle.setFill(Color.BLACK);
+            description.setTextFill(Color.BLACK);
+            cardTitle.setFill(Color.BLACK);
+            cardAttributes.setFill(Color.BLACK);
+        }
+    }
+
+    private void autoSetTextColor(Text text){
+        if (darkModeButton.isSelected()) {
+            text.setFill(Color.WHITE);
+        }
+        else {
+            text.setFill(Color.BLACK);
+        }
+    }
+
+    public void removeCardData(){
         grid.getChildren().removeIf(node -> GridPane.getColumnIndex(node) == 1 && GridPane.getRowIndex(node) == 8);
         grid.getChildren().removeIf(node -> GridPane.getColumnIndex(node) == 1 && GridPane.getRowIndex(node) == 9);
+    }
+
+    private void showError(IOException error){
+        removeCardData();
         cardTitle.setText(error.getMessage());
         cardTitle.setFill(Color.FIREBRICK);
         cardAttributes.setText("");
