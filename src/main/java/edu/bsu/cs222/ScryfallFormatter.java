7089d9512ba53dca006c41d30b21840a3146d89f
North-Kaster <bckaster@bsu.edu>
2022-12-03 14:45:13 -0500

    Replace both em and en dash with hyphen in ScryfallFormatter. Invert if statements

--- src/main/java/edu/bsu/cs222/ScryfallFormatter.java
+++ src/main/java/edu/bsu/cs222/ScryfallFormatter.java
@@ -4,48 +4,37 @@ package edu.bsu.cs222;
 public class ScryfallFormatter {
     public static String formatJson(Card[] cardList) {
 
-
-        //ArrayList<String> cardAttributes = new ArrayList<>();
         String formattedCard = "";
         ColorFormatter colorFormatter = new ColorFormatter();
         ManaFormatter manaFormatter = new ManaFormatter();
 
-        if(cardList[0].getManaCost().isBlank()){
-        } else {
+        if (!cardList[0].getManaCost().isBlank()) {
             formattedCard = "Mana Cost:" + manaFormatter.format(cardList[0].getManaCost());
         }
-        if(cardList[0].getCardType().isBlank()) {
-        } else {
+        if (!cardList[0].getCardType().isBlank()) {
             formattedCard += "\n" + "Type: " + cardList[0].getCardType();
         }
-        if(cardList[0].getCardRarity().isBlank()) {
-        } else {
+        if (!cardList[0].getCardRarity().isBlank()) {
             formattedCard += "\n" + "Rarity: " + cardList[0].getCardRarity();
         }
-        if (cardList[0].getCardAbilities().isBlank()) {
-        } else {
+        if (!cardList[0].getCardAbilities().isBlank()) {
             formattedCard += "\n" + "Abilities: " + cardList[0].getCardAbilities();
         }
-        if (cardList[0].getCardPower().isBlank()){
-        } else {
+        if (!cardList[0].getCardPower().isBlank()) {
             formattedCard += "\n" + "Power: " + cardList[0].getCardPower();
         }
-        if (cardList[0].getCardToughness().isBlank()) {
-        } else {
+        if (!cardList[0].getCardToughness().isBlank()) {
             formattedCard += "\n" + "Toughness: " + cardList[0].getCardToughness();
         }
-        if (colorFormatter.format(cardList[0].getCardColors()).isBlank()) {
-        } else {
+        if (!colorFormatter.format(cardList[0].getCardColors()).isBlank()) {
             formattedCard += "\n" + "Colors: " + colorFormatter.format(cardList[0].getCardColors());
         }
-        if (cardList[0].getCardLoyalty().isBlank()) {
-        } else {
+        if (!cardList[0].getCardLoyalty().isBlank()) {
             formattedCard += "\n" + "Loyalty: " + cardList[0].getCardLoyalty();
         }
-        if (cardList[0].getCardUsd().isBlank()) {
-        } else {
+        if (!cardList[0].getCardUsd().isBlank()) {
             formattedCard += "\n" + "Price (USD): $" + cardList[0].getCardUsd();
         }
-        return formattedCard.replaceAll("—", "-");
+        return formattedCard.replaceAll("—–", "-");
     }
 }
